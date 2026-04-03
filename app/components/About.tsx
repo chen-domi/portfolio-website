ed34599377b213d8ab06b70d562a750ab855be3f
Dominic Chen <cchendominic@gmail.com>
2026-04-03 15:02:12 -0400

    Add to about section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -4,16 +4,16 @@ export const About = () => {
             <section id="about">
                 <div className="mb-8">
                     <h1 className="mt-12 mb-6 text-2xl font-semibold tracking-tighter">Hi, I'm Dom👋</h1>
-                    <p>I'm a sophomore studying Computer Science at Boston College focused on AI product engineering and development. I'm passionate about building innovative products that leverage AI to solve real-world problems.</p>
+                    <p className="mb-16">I'm a sophomore studying Computer Science at Boston College focused on AI product engineering and development. I'm passionate about building innovative products that leverage AI to solve real-world problems.</p>
                 </div>
                 <div className="mb-8">
-                    <h2>Education</h2>
+                    <h2 className="font-bold">Education</h2>
                     <p>Boston College - B.S. Computer Science</p>
-                    <p>2024-2028</p>
+                    {/* <p>2024-2028</p> */}
                 </div>
 
                 <div className="mb-8">
-                    <h2>Interests & Hobbies</h2>
+                    <h2 className="font-bold">Interests & Hobbies</h2>
                     <ul className="list-disc list-inside">
                         <li>Formula 1 - Ferrari</li>
                         <li>NBA - Warriors</li>
