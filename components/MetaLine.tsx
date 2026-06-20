3e8e9395ebe9b8e60525b8d8171c359ea7b4f196
Dominic Chen <cchendominic@gmail.com>
2026-06-20 00:34:27 -0400

    Change major and graduation date

--- components/MetaLine.tsx
+++ components/MetaLine.tsx
@@ -1,8 +1,8 @@
 export default function MetaLine() {
   return (
     <div className="flex justify-between font-mono text-[11.5px] text-ink-soft py-2.5 border-b border-rule-light mb-9 uppercase tracking-wide">
-      <span>B.A./B.S. Computer Science, Boston College</span>
-      <span>Updated June 2026</span>
+      <span>B.S. Computer Science, Boston College</span>
+      <span>Expected Graduation May 2028</span>
     </div>
   );
 }
